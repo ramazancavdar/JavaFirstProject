a0aa357d64a1f20bf263c6a1a12431a3ac9b5486
ramazancavdar <124646914+ramazancavdar@users.noreply.github.com>
2023-02-07 20:09:54 -0600

    This is my second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Clarusway Team!");
+        System.out.println("I created First Java Project and pushed to Github");
     }
 }
